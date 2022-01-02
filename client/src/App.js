6ac6e9bea423b3b3e2eac92c714754bbf29c5933
leejanghe <babybye777@gmail.com>
2022-01-03 04:21:32 +0900

    최종 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 // import RandomNum from'./randomNum/RandomNum';
 import React,{useState} from 'react'
+import './App.css';
 
 function App() {
-const [status, setStatus] = useState(false);
+const [status, setStatus] = useState('1~30 사이의 숫자를 맞춰보세요!');
 const [answer, setAnswer] = useState(0);
 const [righyAnswer, setRightAnser] = useState(Math.ceil(Math.random()*30));
 
@@ -12,38 +13,38 @@ function returnFunc(e){
   e.preventDefault();
   if(+answer === righyAnswer){
     console.log(answer)
-    setStatus("정답입니당")
+    setStatus("정답!!!")
     setTimeout(function(){
       resetFunc();
     }, 2000)
     
   }else if(answer < righyAnswer){
-    setStatus("업업업")
+    setStatus("업 ▲")
   }else{
-    setStatus("다운")
+    setStatus("다운 ▼")
   }
 }
 
 function resetFunc(){
   setRightAnser(Math.ceil(Math.random()*30))
-  setStatus("정답을 맞추시오")
+  setStatus("한번더! 1~30 사이의 숫자를 맞춰보세요!")
+  setAnswer(0);
+}
+
+function changeAnswer(e){
+  setAnswer(e.target.value);
 }
 
 
-
-
-
-
   return (
-    <div className="App">
+    <div class="background-card">
        <form onSubmit={returnFunc}>
-      <h1>숫자를 맞춰 보세요!!!</h1>
-      <p>{righyAnswer}</p>
-      <input 
-            type="number"  max="30" min="1"
-            value={answer} onChange={(e) => setAnswer(e.target.value)} />
-      <button>확인</button>
+      <h1>Up and Down</h1>
       <p>{status}</p>
+      <input 
+      type="number"  max="30" min="1"
+      value={answer} onChange={changeAnswer} />
+      <button>확인</button>
       </form>
     </div>
   );
