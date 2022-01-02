130b98ac29ba299e660edc77a8e725951ea9d1ed
leejanghe <babybye777@gmail.com>
2022-01-02 17:26:54 +0900

    html 작업

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import RandomNum from'./randomNum/RandomNum';
 
 function App() {
+
+
   return (
     <div className="App">
-      <h1>gggg</h1>
+      <h1>숫자를 맞춰 보세요!!!</h1>
+      <p>1부터 30사이의 숫자</p>
+      <RandomNum />
     </div>
   );
 }
---
+++ client/src/randomNum/RandomNum.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function RandomNum() {
+    return (
+        <div>
+            <input type="number"  max="30" min="1"/>
+            <button>확인</button>
+        </div>
+    )
+}
+
+export default RandomNum
