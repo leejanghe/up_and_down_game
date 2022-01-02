e7784ae4c5bedbf735541cd08d13003a7d37a232
leejanghe <babybye777@gmail.com>
2022-01-03 03:24:35 +0900

    기능개발 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
-import './App.css';
-import RandomNum from'./randomNum/RandomNum';
+// import RandomNum from'./randomNum/RandomNum';
+import React,{useState} from 'react'
 
 function App() {
+const [status, setStatus] = useState(false);
+const [answer, setAnswer] = useState(0);
+const [righyAnswer, setRightAnser] = useState(Math.ceil(Math.random()*30));
+
+
+function returnFunc(e){
+  
+  e.preventDefault();
+  if(+answer === righyAnswer){
+    console.log(answer)
+    setStatus("정답입니당")
+    setTimeout(function(){
+      resetFunc();
+    }, 2000)
+    
+  }else if(answer < righyAnswer){
+    setStatus("업업업")
+  }else{
+    setStatus("다운")
+  }
+}
+
+function resetFunc(){
+  setRightAnser(Math.ceil(Math.random()*30))
+  setStatus("정답을 맞추시오")
+}
+
+
+
+
 
 
   return (
     <div className="App">
+       <form onSubmit={returnFunc}>
       <h1>숫자를 맞춰 보세요!!!</h1>
-      <p>1부터 30사이의 숫자</p>
-      <RandomNum />
+      <p>{righyAnswer}</p>
+      <input 
+            type="number"  max="30" min="1"
+            value={answer} onChange={(e) => setAnswer(e.target.value)} />
+      <button>확인</button>
+      <p>{status}</p>
+      </form>
     </div>
   );
 }
